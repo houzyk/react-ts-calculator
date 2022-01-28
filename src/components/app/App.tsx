dfdea3e7808d74867894fc4d6819be00f0c9e042
houzairkoussa <houzairmk@icloud.com>
2022-01-28 06:18:55 +0400

    ADD stylewrapper for calculator component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import './App.css';
+import styled, { css } from "styled-components";
+
+// components
+import Calculator from '../calculator/Calculator.component';
 
 function App() {
   return (
-    <>
-    </>
+    <StyleWrapper>
+      <Calculator />
+    </StyleWrapper>
   );
 }
+
+const styles = css`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #040F16;
+`
+
+const StyleWrapper = styled.div`
+  ${styles}
+`
 
 export default App;
--- src/components/calculator/Calculator.component.tsx
+++ src/components/calculator/Calculator.component.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import {  } from "styled-components";
+import styled, { css } from "styled-components";
 
 const Calculator:React.FC = () => {
   return (
-    <>
-    </>
+    <StyleWrapper>
+    </StyleWrapper>
   );
 }
+
+const styles = css`
+  height: 70%;
+  width: 27%;
+  background-color: #D36135;
+  border-radius: 15px;
+`
+
+const StyleWrapper = styled.div`
+  ${styles}
+`
 
 export default Calculator;
