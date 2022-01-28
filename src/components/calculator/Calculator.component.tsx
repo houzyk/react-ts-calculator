2a7912c37310625419a44f623199bd68dd0ffc2a
houzairkoussa <houzairmk@icloud.com>
2022-01-28 06:01:49 +0400

    ADD npm styled-components + @types

---
+++ src/components/calculator/Calculator.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {  } from "styled-components";
+
+const Calculator:React.FC = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Calculator;
--- src/redux/root-reducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({
-  // todo: add reducers
-});
-
-export default rootReducer;
